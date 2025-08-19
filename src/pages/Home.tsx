cd56066bcd76a5219d18e60c910c4c53af9c1218
Gabriel Oliveira <gabrielscorehero55@gmail.com>
2025-08-19 19:23:29 -0300

    feat: update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,28 @@
 import { useAuth } from "../contexts/AuthContext";
 
 function Home() {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
 
   return (
     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto">
         <div className="text-center">
           <h1 className="text-3xl font-extrabold text-gray-900 mb-8">
-            Welcome to ConectaMente
+            Bem-vindo ao ConectaMente
           </h1>
           <p className="text-gray-600 mb-8">
-            You are successfully logged in to the protected area.
+            Você está logado com sucesso na área protegida.
           </p>
+
+          {
+            user && (
+              <p className="text-gray-600 mb-8">
+                Olá, {user.name} pronto para explorar o ConectaMente!
+              </p>
+            )
+          }
+
+
           <button
             onClick={logout}
             className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
@@ -21,7 +31,7 @@ function Home() {
           </button>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
